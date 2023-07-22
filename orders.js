c114db365c17812f7ceb647198900e7fdd9e4db5
Ivan <djakoviczg@gmail.com>
2023-07-22 08:42:08 +0200

    final

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+      productName: 'iPhone 14 Pro Max 256GB',
+      productNumber: '85743',
+      paymentStatus: 'Due',
+      status: 'Pending'
+  },
+  {
+      productName: 'Samsung S23+ 128GB',
+      productNumber: '97245',
+      paymentStatus: 'Refunded',
+      status: 'Declined'
+  },
+  {
+      productName: 'Google Pixel',
+      productNumber: '36452',
+      paymentStatus: 'Paid',
+      status: 'Active'
+  },
+]
